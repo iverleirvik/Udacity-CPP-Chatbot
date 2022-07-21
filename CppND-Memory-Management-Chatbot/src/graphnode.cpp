1b97f64541e657de20a83c48d01da30f2f65c7e9
iverleirvik <94975465+iverleirvik@users.noreply.github.com>
2022-07-21 21:39:19 +0000

    Add files via upload

---
+++ CppND-Memory-Management-Chatbot/src/graphnode.cpp
@@ -0,0 +1,60 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    //delete _chatBot; 
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
+{
+    _childEdges.push_back(std::move(edge));
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot chatbot)    // Change from pointer to instance.
+{
+    _chatBot = std::move(chatbot);  // Use std::move 
+    _chatBot.SetCurrentNode(this);  // used . because now it is instance.
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(std::move(_chatBot));
+    //_chatBot = nullptr; // invalidate pointer at source
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();    //Use .get() cause of smart pointer.
+
+    ////
+    //// EOF STUDENT CODE
+}
